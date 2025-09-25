ccc1781509af53d063da89cd100af632e41934d8
gamdboa <jorgedani@outlook.es>
2025-09-24 22:46:10 -0600

    ejercicio de Progra semana 2

---
+++ Semana2/ejercicio_programacion.py
@@ -0,0 +1,5 @@
+# Haga un programa que lea una lista de números enteros.
+# Dicha lista será considerada como una función de choque de asteroides.
+# El signo del numero indica la dirección: Si es positivo, va de derecha a izquierda, si es negativo va de izquierda a derecha.
+# Cada vez que hay un positivo seguido de un negativo, o viceversa, hay un choque de asteroides.
+# El “resultado” será la consecuencia de la suma de ambos. Y la lista resultante deberá decir: 0 si el asteroide no choco, o el resultado del choque EN EL MAYOR si lo hubo. 
