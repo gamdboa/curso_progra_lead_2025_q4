1ae104b8b0b42b2417b6dcfb240ef19041e64d4a
gamdboa <jorgedani@outlook.es>
2025-10-05 22:51:31 -0600

    ajuste para json prueba inicial

--- TareaProgra1/tarea1.py
+++ TareaProgra1/tarea1.py
@@ -3,7 +3,16 @@ import modulos as m
 def main():
     archivo=m.extractor_nombre()
     filas, headers=m.lector(archivo)
-    formato=
+    formato=m.formatos()
+    columnas=m.headers(headers)
+    
+    nombre=archivo.rsplit('.',1)[0]
+    if formato=='json':
+        m.a_json(filas,columnas,nombre+'.json')
+    elif formato=='xml':
+        print('hola, aqui va xml')
+    else:
+        print('hola, aqui va trama')
 
 if __name__=='__main__':
     main()
