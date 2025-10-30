7ad3f6a76f89ead8f0653b8f61a9c2848fd40c46
Daniel Gamboa <jorgedani@outlook.es>
2025-10-29 23:10:39 -0600

    ajustes menores

--- TareaProgra2/cesar.py
+++ TareaProgra2/cesar.py
@@ -6,12 +6,12 @@ def encriptado_cesar(texto, desplazado):
         if char.isalpha():
             # convertira posicion (0–25)
             posicion = (ord(char) - ord('a') + desplazado) % 26
-            # convertir de vuelta a letra
+            # convertir de vuelta a letra (usando como base el codigo de 'a')
             resultado += chr(posicion + ord('a'))
         else:
             resultado += char
     return resultado
 
 def decriptrado_cesar(text, desplazado):
-    # Decryption is just shifting in the opposite direction
+    # Solo se devuelve 1 posicion
     return encriptado_cesar(text, -desplazado)
--- TareaProgra2/vigenere.py
+++ TareaProgra2/vigenere.py
@@ -17,9 +17,13 @@ def encriptado_vigenere(texto, clave):
 def decriptrado_vigenere(texto, clave):
     resultado = ""
     largo_clave = len(clave)
+    # enumerate() nos da un contador (i) y la letra actual (char)
     for i, char in enumerate(texto):
         if char.isalpha():
+            # Calculamos cuánto hay que "desplazar" la letra
             desplazado = ord(clave[i % largo_clave]) - ord('a')
+            # char se convierte a numero y resta el desplazamiento (lo que se movio inicialmente)
+            # se usa % 26 para que el conteo sea circular (de 'a' a 'z')
             pos = (ord(char) - ord('a') - desplazado) % 26
             resultado += chr(pos + ord('a'))
         else:
